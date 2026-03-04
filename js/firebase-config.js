e3cbc363cf7e8d79f1ce3c4033eb30a3e24600f0
ayliz <merveayliz2301@gmail.com>
2026-03-04 22:16:36 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8xaNazRZZtKVBZHtmOWhdyRFK-Fk514",
+  authDomain: "ark-os-b0842.firebaseapp.com",
+  projectId: "ark-os-b0842",
+  storageBucket: "ark-os-b0842.firebasestorage.app",
+  messagingSenderId: "667102824523",
+  appId: "1:667102824523:web:96d7d11cdc19e6fb46e5ce",
+  measurementId: "G-LYLXJC71WR"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
